2237f43d6f29c548b9b941b6bcd5251b323dedf6
ll1n090 <nikodempiatek579@gmail.com>
2026-08-23 08:06:37 +0200

    Enable member intents for the bot

--- bot.py
+++ bot.py
@@ -273,6 +273,8 @@ class Bot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
+        intents.members = True
+
         super().__init__(command_prefix="!", intents=intents)
 
     async def setup_hook(self):
